c9869426f55d6a7f1328cbeee1d8f6a13ce12a2f
Omotola Ogunsola <67585135+ArcOmotola@users.noreply.github.com>
2021-08-14 19:28:44 +0100

    Update firebase.js
    secret removed from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB4-PvUBfx3KJX6BaHtP2EwGEUxjfmE25w",
-    authDomain: "linked-in-clone-64a95.firebaseapp.com",
-    projectId: "linked-in-clone-64a95",
-    storageBucket: "linked-in-clone-64a95.appspot.com",
-    messagingSenderId: "290430848765",
-    appId: "1:290430848765:web:7ac36517c2daf52914f894",
-    measurementId: "G-2JT7K8RSHJ"
+    ***************************secret*******
     };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
